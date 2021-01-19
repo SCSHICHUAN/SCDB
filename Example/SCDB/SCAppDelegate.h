203f4eea2616ded02f4f1a2043f932525c9148a3
Stan <573387383@QQ.COM>
2021-01-19 17:35:39 +0800

    Initial commit

---
+++ Example/SCDB/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SCDB
+//
+//  Created by scshichuan on 01/19/2021.
+//  Copyright (c) 2021 scshichuan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/SCDB/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  SCDB
+//
+//  Created by scshichuan on 01/19/2021.
+//  Copyright (c) 2021 scshichuan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SCViewController : UIViewController
+
+@end
